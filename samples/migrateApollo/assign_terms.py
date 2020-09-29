75d4a1f30a3d99018a3ce5a9584d6e09e2542052
Will Johnson <wijohns@microsoft.com>
2020-09-28 21:14:10 -0500

    Completed relationship mapping

--- samples/migrateApollo/assign_terms.py
+++ samples/migrateApollo/assign_terms.py
@@ -37,12 +37,15 @@ if __name__ == "__main__":
     if args.sheetname:
         required_sheet = args.sheetname
 
+    print(f"Reading {args.file_path} and sheet {args.sheetname}")
+
     data_sheet = wb.get_sheet_by_name(required_sheet)
 
     # Iterate over each row, could do this in pandas but want to avoid
     # the dependency
 
     # Getting the headers
+    print("Beginning reading sheet")
     sheet_headers = []
     null_threshold = 0.5
     for row in data_sheet.iter_rows(min_row=1, max_col=data_sheet.max_column, max_row=1):
@@ -55,20 +58,21 @@ if __name__ == "__main__":
         row_data = {}
         nulls = 0
         for idx, cell in enumerate(row):
-            row_data[sheet_headers[idx]] = cell.value
+            row_data[sheet_headers[idx]] = (cell.value or "").strip()
             if cell.value is None:
                 nulls = nulls + 1
-        
+
         if float(nulls) / float(len(row_data)) < null_threshold:
             sheet_data.append(row_data)
 
     # Extract the following fields:
-    key_headers = [
-        "fld_it_name", "fld_business_name",
-        "tbl_it_name", "tbl_business_name"
-    ]
+    # key_headers = [
+    #     "fld_it_name", "fld_business_name",
+    #     "tbl_it_name", "tbl_business_name"
+    # ]
 
     # Find the unique terms (*_business_name)
+    print("Finding the terms associated with each entity")
     term_guids = dict()
     known_entities = set()
     # TODO: Find the qualifiedName for the table and column
@@ -80,61 +84,88 @@ if __name__ == "__main__":
 
         known_entities.add(table_name)
         known_entities.add(column_name)
-        
+
         if table_term not in term_guids:
-            term_guids[table_term] = {"guid":None, "qualifiedNames":set()}
+            term_guids[table_term] = {"guid": None, "qualifiedNames": set()}
         if column_term not in term_guids:
-            term_guids[column_term] = {"guid":None, "qualifiedNames":set()}
-        
+            term_guids[column_term] = {"guid": None, "qualifiedNames": set()}
+
         term_guids[table_term]["qualifiedNames"].add(table_name)
         term_guids[column_term]["qualifiedNames"].add(column_name)
-    
-    
+
+    print(f"Found {len(term_guids)} term(s) and {len(known_entities)} entities.")
+
     # Look up the terms necessary
+    print("Downloading the default glossary")
     glossary = atlas_client.get_glossary()
 
-    for term in glossary["terms"]:
-        current_term_value = term["displayText"]
-        if current_term_value in term_guids:
-            term_guids[current_term_value]["guid"] = term["termGuid"]
-    
+    # Convert into a dictionary rather than a list
+    glossary_lookup = {
+        term["displayText"]: term["termGuid"]
+        for term in glossary["terms"]
+    }
 
+    # Try to find the guid for each term
     for term in term_guids:
-        term_guids[term]["qualifiedNames"] = list(term_guids[term]["qualifiedNames"])
-    
-    print(json.dumps(term_guids))
-    
-    print(len(term_guids))
+        if term not in glossary_lookup:
+            print(f"Warning: {term} not found in glossary")
+            continue
+        term_guids[term]["guid"] = glossary_lookup[term]
+        term_guids[term]["qualifiedNames"] = list(
+            term_guids[term]["qualifiedNames"])
 
-    # TODO: Flesh out the search query loop
+    print(
+        f"There were {sum([('guid' in term) for term in term_guids.values()])} guids found for {len(term_guids)} terms.")
+
     # For each column and table, find their entity id
-    # qualified_name_lookup = dict()
-    # for qualifiedName in known_entities:
-    #     search_results = atlas_client.search_entities(query=qualifiedName)
-    #     for batch in search_results:
-    #         for entity in batch:
-    #             if entity["qualifiedName"] == qualifiedName:
-    #                 qualified_name_lookup[qualifiedName] = entity["guid"]
-    #                 break
-    #         if qualifiedName in qualified_name_lookup:
-    #             break
-    
+    print("Looking up the known entities")
+    qualified_entity_name_lookup = dict()
+    for qualifiedName in known_entities:
+        search_results = atlas_client.search_entities(query=qualifiedName)
+        for batch in search_results:
+            for entity in batch:
+                if entity["qualifiedName"] == qualifiedName:
+                    # Search results looks at "id" and not "guid"
+                    qualified_entity_name_lookup[qualifiedName] = entity["id"]
+                    break
+            if qualifiedName in qualified_entity_name_lookup:
+                break
+
+    print(
+        f"There were {len(qualified_entity_name_lookup)} entities found out of {len(known_entities)} known entities.")
+
     # TODO: Create the relationship between term_guids and the entities
-    # for term in term_guids:
-    #     for qualified_name in term["qualifiedNames"]:
-    #         if qualified_name not in qualified_name_lookup:
-    #             print(f"Warning: {qualified_name} was not found in any entities in the catalog.")
-    #             break
-    #         entity_guid = qualified_name_lookup[qualified_name]
-    #         # TODO: Define a relationship
-    #         relationship = {}
-    #         try:
-    #             atlas_client.upload_relationship(relationship)
-    #         except Exception as e:
-    #             print(f"Exception for {term}:{qualified_name} and was not uploaded: {e}")
-        
-
-
+    for term_name, term_dict in term_guids.items():
+        for entity_qualified_name in term_dict["qualifiedNames"]:
+            if entity_qualified_name != "nme_clmt_full":
+                continue
+            print(f"{term_name}:{entity_qualified_name}")
+            if entity_qualified_name not in qualified_entity_name_lookup:
+                print(
+                    f"Warning: {entity_qualified_name} was not found in any entities in the catalog.")
+                break
+            term_guid = term_dict["guid"]
+            term_qualified_name = f"{term_name}@Glossary"
+            entity_guid = qualified_entity_name_lookup[entity_qualified_name]
             
-                
-
+            relationship = {
+                "typeName": "AtlasGlossarySemanticAssignment",
+                "attributes": {},
+                "guid": -100,
+                "end1": {
+                    "guid": term_guid,
+                    "typeName": "AtlasGlossaryTerm",
+                    "uniqueAttributes": {
+                            "qualifiedName": term_qualified_name
+                    }
+                },
+                "end2": {
+                    "guid": entity_guid
+                }
+            }
+            try:
+                results = atlas_client.upload_relationship(relationship)
+                print("\tSuccess")
+            except Exception as e:
+                print(
+                    f"Exception for {term}:{entity_qualified_name} and was not uploaded: {e}")
