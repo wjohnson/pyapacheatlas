b27d2f53e9f7de93da803fe4368e9fdf051edc55
Will Johnson <wijohns@microsoft.com>
2021-01-16 23:14:29 -0600

    Adding support for relationshipAttributes and automagic conversion of AtlasEntity.
    Now, a user can call addRelationship on an AtlasEntity and not have to worry about
    remembering to call to_json(minimum=True) on the AtlasEntity.

---
+++ tests/integration/EntityREST/test_entity_upload.py
@@ -0,0 +1,67 @@
+import json
+import os
+import time
+
+from pyapacheatlas.auth import ServicePrincipalAuthentication
+from pyapacheatlas.core.client import PurviewClient
+from pyapacheatlas.core import AtlasEntity
+from pyapacheatlas.core.typedef import EntityTypeDef
+
+oauth = ServicePrincipalAuthentication(
+    tenant_id=os.environ.get("TENANT_ID", ""),
+    client_id=os.environ.get("CLIENT_ID", ""),
+    client_secret=os.environ.get("CLIENT_SECRET", "")
+)
+client = PurviewClient(
+    account_name = os.environ.get("PURVIEW_NAME", ""),
+    authentication=oauth
+)
+
+def test_set_relationship_different_ways():
+
+    ae = AtlasEntity("rel01","hive_table", "tests://rel01", guid=-1)
+    c1 = AtlasEntity("rel01#01", "hive_column", "tests://rel01#c", guid=-2, attributes={"type":"str"})
+    c2 = AtlasEntity("rel01#02", "hive_column", "tests://rel02#c", guid=-3, attributes={"type":"str"})
+    c3 = AtlasEntity("rel01#03", "hive_column", "tests://rel03#c", guid=-4, attributes={"type":"str"})
+    c4 = AtlasEntity("rel01#04", "hive_column", "tests://rel04#c", guid=-5, attributes={"type":"str"})
+
+    # Add c1 as the only relationship
+    ae.addRelationship(columns=[c1.to_json(minimum=True)])
+
+    c2.relationshipAttributes.update({"table": ae.to_json(minimum=True) })
+    c3.addRelationship(table = ae)
+
+    assignments = client.upload_entities([ae, c1, c2, c3, c4])["guidAssignments"]
+    try:
+        live_table = client.get_entity(guid=assignments["-1"])["entities"][0]
+        
+        # Should have two attributes because one is from the table having the
+        # relationship defined as an array of columns and the second two from
+        # the column's having the table relationshipAttribute defined on them.
+        assert(len(live_table["relationshipAttributes"]["columns"]) == 3)
+
+        relationship = {
+                    "typeName": "hive_table_columns",
+                    "attributes": {},
+                    "guid": -100,
+                    # Ends are either guid or guid + typeName 
+                    # (in case there are ambiguities?)
+                    "end1": {
+                        "guid": assignments["-1"]
+                    },
+                    "end2": {
+                        "guid": assignments["-5"]
+                    }
+                }
+
+        relation_upload = client.upload_relationship(relationship)
+        # Check that we have one more relationship
+        # There are caching issues here :-(
+        live_table_post_relationship = client.get_entity(guid=assignments["-1"])["entities"][0]
+        assert(len(live_table["relationshipAttributes"]["columns"]) == 4)
+
+    finally:
+        # Need to delete all columns BEFORE you delete the table
+        for local_id in [str(s) for s in range(-5,0)]:
+            guid = assignments[local_id]
+            _ = client.delete_entity(guid)
