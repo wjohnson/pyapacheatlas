5d16512ec31436ad62105f3881994f786b0b3505
wjohnson <will@willj.co>
2021-02-23 16:35:33 -0600

    Adding delete instrutions too

--- samples/CRUD/create_type_definition.py
+++ samples/CRUD/create_type_definition.py
@@ -3,7 +3,7 @@ import json
 
 from pyapacheatlas.auth import ServicePrincipalAuthentication
 from pyapacheatlas.core import AtlasAttributeDef, AtlasEntity, PurviewClient
-from pyapacheatlas.core.typedef import EntityTypeDef
+from pyapacheatlas.core.typedef import EntityTypeDef, TypeCategory
 
 
 if __name__ == "__main__":
@@ -22,29 +22,44 @@ if __name__ == "__main__":
         authentication=oauth
     )
 
+    # Create an entity type definition with three columns (column1, 2, 3)
+    # with column1 required.
     edef = EntityTypeDef(
-        name="pyapacheatlas_custom_type",
+        name="pyapacheatlas_create_type_def_sample",
         attributeDefs=[
-            AtlasAttributeDef("column1", typeName="string"),
+            AtlasAttributeDef("column1", typeName="string", isOptional=False),
             AtlasAttributeDef("column2", typeName="int"),
             AtlasAttributeDef("column3", typeName="array<string>", cardinality="SET"),
         ],
         superTypes=["DataSet"]
     )
 
+    # Do the upload
     results = client.upload_typedefs(
         entityDefs=[edef],
         force_update=True
     )
 
+    # Just for demonstration purposes, get the entity type def.
+    get_results = client.get_typedef(TypeCategory.ENTITY, name="pyapacheatlas_create_type_def_sample")
+    print("# Results from getting created type def:")
+    print(json.dumps(get_results,indent=2))
+
+    # Creating an instance of this custom type
     actual_entity = AtlasEntity(
-        name="a_sample_entity_of_pyapacheatlas_custom_type",
-        qualified_name="pyapacheatlas://sample_pyapacheatlas_custom_type",
-        typeName="pyapacheatlas_custom_type",
+        name="instance_of_pyapacheatlas_create_type_def_sample",
+        qualified_name="pyapacheatlas://instance_of_pyapacheatlas_create_type_def_sample",
+        typeName="pyapacheatlas_create_type_def_sample",
         attributes={"column1": "abc", "column2": 123, "column3": ["a", "b"]},
         guid=-100
     )
 
     upload_results = client.upload_entities(actual_entity)
 
+    print("# Results of entity upload:")
     print(json.dumps(upload_results, indent=2))
+
+    # To remove, delete the entity created and then the entity type.
+    # client.delete_entity(guid=["..."])
+    # delete_results = client.delete_type("pyapacheatlas_create_type_def_sample")
+    # print(json.dumps(delete_results, indent=2))
