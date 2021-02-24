c6a8f28a3bb4ee39f652d2722713af75231791c9
wjohnson <will@willj.co>
2021-02-24 15:16:56 -0600

    WIP

--- samples/migrateADCGen1/assetmapper.py
+++ samples/migrateADCGen1/assetmapper.py
@@ -1,21 +1,35 @@
 from abc import ABC, abstractproperty
 import json
 
+from pyapacheatlas.core import AtlasEntity
+
+class AssetFactory():
+    @staticmethod
+    def map_asset(asset):
+        dataSource = asset.get("content", {}).get("properties",{}).get("dataSource", {})
+        sourceType = dataSource.get("sourceType", "").lower()
+        objectType = dataSource.get("objectType", "").lower()
+        if sourceType == "" or objectType == "":
+            raise ValueError(f"DataSource not supported: {dataSource}")
+        if sourceType == "sql server" and objectType == "table":
+            return SqlServerTableMapper(asset)
+        else:
+            raise ValueError(f"DataSource not supported: {dataSource}")
+
+
 class AssetMapper(ABC):
 
     def __init__(self, asset):
         self.asset = asset["content"]
-    
-    @abstractproperty
-    def qualified_name(self):
-        raise NotImplementedError
-
-    @abstractproperty
-    def relationships(self):
-        raise NotImplementedError
 
     @property
     def annotations(self):
+        # Relevant Annotations
+        ## columnDescriptions
+        ## termTags
+        ## tags
+        ## schema.properties.columns
+        ## friendlyName 
         return self.asset["annotations"]
     
     @property
@@ -31,7 +45,36 @@ class AssetMapper(ABC):
         return self.properties["name"]
 
 class SqlServerTableMapper(AssetMapper):
-    
+    # columnDescriptions
+    def _define_column_descriptions(self):
+        # Check if columnDescriptions is defined
+        # Get schema
+        # Create objects
+        # Apply description
+        return None
+    # documentation
+    # termTags
+    def entities(self):
+        output = []
+        server = AtlasEntity()
+        database = AtlasEntity()
+        schema = AtlasEntity()
+        table = AtlasEntity(
+            name= self.name,
+            typeName="azure_sql_table",
+            qualified_name=self.qualified_name(),
+            guid=None
+        )
+        columns = []
+        for col in self.schema:
+            columns.append(
+                AtlasEntity()
+            )
+        return 
+    # schema
+    # tags
+    # friendlyName
+    # descriptions
     def __init__(self, asset):
         super().__init__(asset)
         _address = self.properties["dsl"]["address"]
@@ -40,24 +83,29 @@ class SqlServerTableMapper(AssetMapper):
         self.schema = _address["schema"]
         self.table = _address["object"]
     
-    @property
-    def qualified_name(self):
-        return f"mssql://{self.server}/{self.database}/{self.schema}/{self.table}"
-    
-    @property
-    def relationships(self):
-        output = {
-            "columns": self.annotations.get("schema", {}).get("properties", {}).get("columns", []),
-            "dboSchema": f"mssql://{self.server}/{self.database}/{self.schema}"
-        }
+    def qualified_name(self, level="table"):
+        output = f"mssql://{self.server}"
+        if level in ["database", "schema", "table"]:
+            output = output + "/" + self.database
+        if level in ["schema", "table"]:
+            output = output + "/" + self.schema
+        if level in ["table"]:
+            output = output + "/" + self.table
+
         return output
 
-    
-
 if __name__ == "__main__":
-    with open("./test.json", 'r') as fp:
+    with open("./test4.json", 'r') as fp:
         data = json.load(fp)
-    mapped = SqlServerTableMapper(data[5])
-    print(mapped.qualified_name)
-    print(mapped.relationships)
     
+    output = []
+    for d in data:
+        try:
+            _temp = AssetFactory.map_asset(d)
+            output.append(_temp)
+        except ValueError as e:
+            print(e)
+            pass
+    
+    print([o.annotations.keys() for o in output])
+        
