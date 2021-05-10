bb1a7005f2a5db3e8eea3155ad81f41958c98521
Will Johnson <will@willj.co>
2021-05-10 12:09:22 -0500

    V0.6.0 release (#120)

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ from pyapacheatlas import __version__
 LONG_DESCRIPTION = """
 # PyApacheAtlas: API Support for Azure Purview and Apache Atlas
 
-A python package to work with the Apache Atlas API and support bulk loading, custom lineage, and more from a Pythonic set of classes and Excel templates. 
+A python package to work with the Azure Purview and Apache Atlas API. Supporting bulk loading, custom lineage, and more from a Pythonic set of classes and Excel templates.
 
-The package currently supports:
-* Bulk upload of entities.
-* Bulk upload of type definitions.
+The package supports programmatic interaction and an Excel template for low-code uploads.
+
+The Excel template provides a means to:
+* Bulk upload entities
+  * Supports adding glossary terms to entities.
+  * Supports adding classifications to entities.
+  * Supports creating relationships between entities (e.g. columns of a table).
 * Creating custom lineage between two existing entities.
+* Bulk upload of type definitions.
+* Bulk upload of classification definitions (Purview Classification rules are not currently supported).
 * Creating custom table and complex column level lineage in the [Hive Bridge style](https://atlas.apache.org/0.8.3/Bridge-Hive.html).
   * Supports Azure Purview ColumnMapping Attributes.
-* Creating a column lineage scaffolding as in the Hive Bridge Style .
-* Performing "What-If" analysis to check if...
-   * Your entities are valid types.
-   * Your entities are missing required attributes.
-   * Your entities are using undefined attributes.
+
+The PyApacheAtlas package itself supports those operations and more for the advanced user:
+* Programmatically create Entities, Types (Entity, Relationship, etc.).
+* Perform partial updates of an entity (for non-complex attributes like strings or integers).
+* Extracting entities by guid or qualified name.
+* Creating custom lineage with Process and Entity types.
 * Working with the glossary.
   * Uploading terms.
   * Downloading individual or all terms.
@@ -27,8 +34,12 @@ The package currently supports:
 * Working with relationships.
   * Able to create arbitrary relationships between entities.
   * e.g. associating a given column with a table.
-  * Able to upload relationship definitions.
 * Deleting types (by name) or entities (by guid).
+* Creating a column lineage scaffolding as in the Hive Bridge Style .
+* Performing "What-If" analysis to check if...
+   * Your entities are valid types.
+   * Your entities are missing required attributes.
+   * Your entities are using undefined attributes.
 * Search (only for Azure Purview advanced search).
 * Authentication to Azure Purview via Service Principal.
 * Authentication using basic authentication of username and password for open source Atlas.
