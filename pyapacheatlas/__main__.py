6de2381dacc5a7c29e6f328a6b657e147515dc73
Will Johnson <will@willj.co>
2021-08-11 11:41:48 -0500

    Supporting a Configurable Excel Template
    Closes #124 with support for kwargs and handling every sheet
    name and prefixes.
    
    Closes #143 with support for executing a command on the cli to
    create the template and pass in a configuration file to the
    make_template method. Execute the command via:
    `python -m pyapacheatlas --make-template .\some.xlsx` or you
    can pass in a config file with `--config config.ini`.

--- pyapacheatlas/__main__.py
+++ pyapacheatlas/__main__.py
@@ -2,15 +2,25 @@ import argparse
 import configparser
 
 if __name__ == "__main__":
+    """
+    This is an experimental feature that will allow users to make the excel
+    template directly from the CLI rather than instantiating an ExcelReader
+    in the Python shell.
+
+    Use `--config` to provide a config.ini file with a `[DEFAULT]` section
+    header and specify any customizations. See `ExcelReader.make_template`
+    for list of possible parameters / configuration settings.
+    """
     from .readers import ExcelConfiguration, ExcelReader
     from . import __version__
     parser = argparse.ArgumentParser()
     parser.add_argument("--make-template", help="Indicates where the excel template should be created")
     parser.add_argument("-c", "--config", help="The location of the configuration file")
+    parser.add_argument("-cs", "--config-section", help="The config file's section header to be used. Defaults to DEFAULT", default="DEFAULT")
     parser.add_argument("--version", help="Display the version of your PyApacheAtlas package",action="store_true")
     args = parser.parse_args()
 
-    config = None
+    config = {}
     if args.version:
         print(__version__)
         exit(0)
@@ -20,5 +30,5 @@ if __name__ == "__main__":
         config.read(args.config)
 
     if args.make_template:
-        print('Made it here')
-        ExcelReader.make_template(args.make_template)
+        ExcelReader.make_template(args.make_template, **config[args.config_section])
+        print(f"Template successfully written to {args.make_template}")
