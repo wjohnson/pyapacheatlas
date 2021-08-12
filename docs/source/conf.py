320c8d0ac75aa659f6cd517ca85ef5df6242a9bd
Will Johnson <will@willj.co>
2021-08-12 15:37:20 -0500

    Fixing docs version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Will Johnson'
 author = 'Will Johnson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
