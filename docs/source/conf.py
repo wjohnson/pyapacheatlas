081bcb978cc2d07a8b2299b89c3830616846fa0e
Will Johnson <will@willj.co>
2022-06-17 00:23:07 -0500

    Preparing for 0.13.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Will Johnson'
 author = 'Will Johnson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.12.0'
+release = '0.13.0'
 
 
 # -- General configuration ---------------------------------------------------
